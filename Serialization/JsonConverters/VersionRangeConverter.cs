1946be3d99ff9019ccf9c52501036560f2cb54ec
yoaverez <yoav.erez.01@gmail.com>
2024-03-20 10:46:03 +0200

    Implemented the json converters
    Add documentation due to PR

--- Serialization/JsonConverters/VersionRangeConverter.cs
+++ Serialization/JsonConverters/VersionRangeConverter.cs
@@ -1,11 +1,26 @@
 ﻿using NuGet.Versioning;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Serialization.JsonConverters
 {
     /// <summary>
     /// A json converter for the <see cref="VersionRange"/>.
     /// </summary>
-    public class VersionRangeConverter
+    public class VersionRangeConverter : JsonConverter<VersionRange>
     {
+        /// <inheritdoc/>
+        public override VersionRange? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return VersionRange.Parse(reader.GetString()!);
+        }
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, VersionRange value, JsonSerializerOptions options)
+        {
+            // In this project there is only need in deserialization.
+            // Therefore the Write method is not implemented.
+            throw new NotImplementedException();
+        }
     }
 }
