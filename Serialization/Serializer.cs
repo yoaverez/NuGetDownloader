3a93321fc046a245c4521fe9283cacff8494b9c3
yoaverez <yoav.erez.01@gmail.com>
2024-03-20 11:19:47 +0200

    Add empty file for the Serializer

---
+++ Serialization/Serializer.cs
@@ -0,0 +1,9 @@
+﻿namespace Serialization
+{
+    /// <summary>
+    /// A class for serialization and deserialization of objects.
+    /// </summary>
+    public static class Serializer
+    {
+    }
+}
