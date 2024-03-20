2e980bda63d037ee80d317b86e36e5d667c9c9e9
yoaverez <yoav.erez.01@gmail.com>
2024-03-20 10:46:03 +0200

    Implemented the json converters
    Add documentation due to PR

--- Serialization/JsonConverters/NuGetVersionConverter.cs
+++ Serialization/JsonConverters/NuGetVersionConverter.cs
@@ -1,11 +1,26 @@
 ﻿using NuGet.Versioning;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Serialization.JsonConverters
 {
     /// <summary>
     /// A json converter for the <see cref="NuGetVersion"/>.
     /// </summary>
-    public class NuGetVersionConverter
+    public class NuGetVersionConverter : JsonConverter<NuGetVersion>
     {
+        /// <inheritdoc/>
+        public override NuGetVersion? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return NuGetVersion.Parse(reader.GetString()!);
+        }
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, NuGetVersion value, JsonSerializerOptions options)
+        {
+            // In this project there is only need in deserialization.
+            // Therefore the Write method is not implemented.
+            throw new NotImplementedException();
+        }
     }
 }
