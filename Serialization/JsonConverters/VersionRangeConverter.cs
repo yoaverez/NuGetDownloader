d68ba18cac43dbd1d6f88de61bb203b9714f3c21
yoaverez <yoav.erez.01@gmail.com>
2024-03-20 10:45:40 +0200

    Add empty files for the json converters

---
+++ Serialization/JsonConverters/VersionRangeConverter.cs
@@ -0,0 +1,11 @@
+﻿using NuGet.Versioning;
+
+namespace Serialization.JsonConverters
+{
+    /// <summary>
+    /// A json converter for the <see cref="VersionRange"/>.
+    /// </summary>
+    public class VersionRangeConverter
+    {
+    }
+}
