341e4b7c00abe7a010b0dd5c418f9fe30863e220
yoaverez <yoav.erez.01@gmail.com>
2024-03-20 10:59:18 +0200

    Implemented the DefaultSerializationSettings

--- Serialization/DefaultSerializationSettings.cs
+++ Serialization/DefaultSerializationSettings.cs
@@ -1,9 +1,23 @@
-﻿namespace Serialization
+﻿using Serialization.JsonConverters;
+using System.Text.Json;
+
+namespace Serialization
 {
     /// <summary>
     /// A class for creating default serialization settings for the project.
     /// </summary>
     public static class DefaultSerializationSettings
     {
+        /// <returns>A new <see cref="JsonSerializerOptions"/> with a default settings for this project.</returns>
+        public static JsonSerializerOptions GetDefaultSerializationSettings()
+        {
+            var serializationOptions = new JsonSerializerOptions();
+
+            // Add custom converters.
+            serializationOptions.Converters.Add(new NuGetVersionConverter());
+            serializationOptions.Converters.Add(new VersionRangeConverter());
+
+            return serializationOptions;
+        }
     }
 }
