d767fe85add6e4c2c6f369dcabe02e520001d0e4
yoaverez <yoav.erez.01@gmail.com>
2024-03-20 14:59:39 +0200

    Implemented the dtos

--- NuGetDTOs/PackageMetaData.cs
+++ NuGetDTOs/PackageMetaData.cs
@@ -1,6 +1,25 @@
-﻿namespace Playground.NuGetDTOs
+﻿using NuGet.Versioning;
+using Playground.NuGetDTO;
+using System.Text.Json.Serialization;
+
+namespace Playground.NuGetDTOs
 {
-    public record PackageMetaData
+    /// <summary>
+    /// A DTO for the api's "Catalog entry" object.<br/>
+    /// Represents a package's (with a specific version) meta data.
+    /// </summary>
+    /// <param name="ThisPackageMetaDataUrl">The url from which you can get this package meta data.</param>
+    /// <param name="PackageId">The id of the package (usually the package name).</param>
+    /// <param name="PackageVersion">The version of the package.</param>
+    /// <param name="DependencyGroups">
+    /// An enumerable of the package dependencies groups.<br/>
+    /// Each group represents the dependencies of different target framework e.g. net7 or net4.8.<br/>
+    /// This may be null or an empty enumerable if there are no dependencies.
+    /// </param>
+    public record PackageMetaData([property: JsonPropertyName("@id")] string ThisPackageMetaDataUrl,
+                                  [property: JsonPropertyName("id")] string PackageId,
+                                  [property: JsonPropertyName("version")] NuGetVersion PackageVersion,
+                                  [property: JsonPropertyName("dependencyGroups")] IEnumerable<DependencyGroup>? DependencyGroups)
     {
     }
 }
--- NuGetDTOs/PackageVersion.cs
+++ NuGetDTOs/PackageVersion.cs
@@ -1,6 +1,15 @@
-﻿namespace Playground.NuGetDTOs
+﻿using NuGet.Versioning;
+using System.Text.Json.Serialization;
+
+namespace Playground.NuGetDTOs
 {
-    public record PackageVersion
+    /// <summary>
+    /// A DTO representing a specific package version info.
+    /// </summary>
+    /// <param name="RegistrationLeafUrl">A url that leads to the specific package version <see cref="RegistrationLeaf"/>.</param>
+    /// <param name="Version">The version of the package.</param>
+    public record PackageVersion([property: JsonPropertyName("@id")] string RegistrationLeafUrl,
+                                 [property: JsonPropertyName("version")] NuGetVersion Version)
     {
     }
 }
