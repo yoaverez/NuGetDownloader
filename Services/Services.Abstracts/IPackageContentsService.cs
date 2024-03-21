aeb406952378655ce2a2eb80bbbe91156d59867f
yoaverez <yoav.erez.01@gmail.com>
2024-03-21 14:47:19 +0200

    Implemented all the Services.Abstract files
    Remove the serviceBaseUrl field from the HttpService
    
    Allow the option to not have a base url
    
    Fix documentation of the HttpService

--- Services/Services.Abstracts/IPackageContentsService.cs
+++ Services/Services.Abstracts/IPackageContentsService.cs
@@ -1,6 +1,20 @@
-﻿namespace Services.Abstracts
+﻿using NuGet.Versioning;
+
+namespace Services.Abstracts
 {
+    /// <summary>
+    /// A service for the retrieval of a package contents i.e. the contents of the .nupkg file.
+    /// </summary>
     public interface IPackageContentsService
     {
+        /// <summary>
+        /// Download the package with the given <paramref name="packageName"/> and <paramref name="packageVersion"/>
+        /// to the given <paramref name="outputDirectory"/>.
+        /// </summary>
+        /// <param name="packageName">The name of the package to download.</param>
+        /// <param name="packageVersion">The wanted version of the package.</param>
+        /// <param name="outputDirectory">The directory in which to place the package file.</param>
+        /// <returns>A task that downloads the wanted package to the given <paramref name="outputDirectory"/>.</returns>
+        Task DownloadPackageAsync(string packageName, NuGetVersion packageVersion, string outputDirectory);
     }
 }
--- Services/Services.Abstracts/IServiceFactory.cs
+++ Services/Services.Abstracts/IServiceFactory.cs
@@ -1,6 +1,17 @@
 ﻿namespace Services.Abstracts
 {
+    /// <summary>
+    /// A contract for factories that create services.
+    /// </summary>
     public interface IServiceFactory
     {
+        /// <returns>A an instance of the <see cref="ISearchService"/> class.</returns>
+        ISearchService GetSearchService();
+
+        /// <returns>A an instance of the <see cref="IPackageContentsService"/> class.</returns>
+        IPackageContentsService GetPackageContentsService();
+
+        /// <returns>A an instance of the <see cref="IPackageMetaDataService"/> class.</returns>
+        IPackageMetaDataService GetPackageMetaDataService();
     }
 }
