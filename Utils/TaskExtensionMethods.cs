69697d9407152ea82a230bebfbc24f9698d14a10
yoaverez <yoav.erez.01@gmail.com>
2024-03-21 15:23:57 +0200

    Implemented the Utils files

--- Utils/TaskExtensionMethods.cs
+++ Utils/TaskExtensionMethods.cs
@@ -1,6 +1,20 @@
 ﻿namespace Utils
 {
+    /// <summary>
+    /// Extension method for the <see cref="Task"/> class.
+    /// </summary>
     public static class TaskExtensionMethods
     {
+        /// <summary>
+        /// A waiter for this task instance.
+        /// </summary>
+        /// <typeparam name="T">The return type the task to wait.</typeparam>
+        /// <param name="task">The task to wait.</param>
+        /// <returns>The result of this task instance.</returns>
+        public static T TaskWaiter<T>(this Task<T> task)
+        {
+            task.Wait();
+            return task.Result;
+        }
     }
 }
