5dce9f0a27c92e7c070bfcdac10185ceeaf30b26
yoaverez <yoav.erez.01@gmail.com>
2024-03-22 13:21:43 +0200

    Implemented the LoggersFactory

--- Logs/Logs.Factories/LoggersFactory.cs
+++ Logs/Logs.Factories/LoggersFactory.cs
@@ -1,9 +1,20 @@
-﻿namespace Logs.Factories
+﻿using Logs.Abstracts;
+using Logs.Internals;
+
+namespace Logs.Factories
 {
     /// <summary>
     /// A class for creating loggers.
     /// </summary>
     public static class LoggersFactory
     {
+        /// <summary>
+        /// Create new logger.
+        /// </summary>
+        /// <param name="appsettingsPath">The path to the logger configuration file.</param>
+        public static ILogger CreateLogger(string appsettingsPath)
+        {
+            return new SerilogLogger(appsettingsPath);
+        }
     }
 }
