330425f209d16873e725013cbd26cdb525c449be
yoaverez <yoav.erez.01@gmail.com>
2024-04-06 12:27:37 +0300

    Add empty files for the command line options

---
+++ NuGetDownloader/CommandLineObjects/Options/GenerateConfigOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace NuGetDownloader.CommandLineObjects.Options
+{
+    /// <summary>
+    /// A command line verb for generating an example of an the application configuration
+    /// i.e. it's appsetting file.
+    /// </summary>
+    [Verb("generate-config", HelpText = "Generate an example of how the application configuration file (i.e. appsettings) should look like.")]
+    public class GenerateConfigOptions
+    {
+    }
+}
