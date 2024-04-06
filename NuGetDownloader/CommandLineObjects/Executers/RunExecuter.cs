99b80f142301b4c6062132e3c8c9c8f81b97ac21
yoaverez <yoav.erez.01@gmail.com>
2024-04-06 12:31:25 +0300

    Add empty files for the command line executers

---
+++ NuGetDownloader/CommandLineObjects/Executers/RunExecuter.cs
@@ -0,0 +1,9 @@
+﻿namespace NuGetDownloader.CommandLineObjects.Executers
+{
+    /// <summary>
+    /// An executer for the command line "run" verb.
+    /// </summary>
+    public static class RunExecuter
+    {
+    }
+}
