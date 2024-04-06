8ad042818e9d1065d415d056191482d4e5374a15
yoaverez <yoav.erez.01@gmail.com>
2024-04-06 12:27:37 +0300

    Add empty files for the command line options

---
+++ NuGetDownloader/CommandLineObjects/Options/RunOptions.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace NuGetDownloader.CommandLineObjects.Options
+{
+    /// <summary>
+    /// A command line options for the "run" verb.
+    /// </summary>
+    [Verb("run", HelpText = "Download the wanted packages and their dependencies.")]
+    public class RunOptions
+    {
+    }
+}
