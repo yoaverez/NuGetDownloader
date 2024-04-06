72523c752be788159ff0a80088653cfb3946b1c8
yoaverez <yoav.erez.01@gmail.com>
2024-04-06 12:22:50 +0300

    Implemented the project configuration files

--- NuGetDownloader/Configuration/DownloadRequest.cs
+++ NuGetDownloader/Configuration/DownloadRequest.cs
@@ -5,5 +5,14 @@
     /// </summary>
     public class DownloadRequest
     {
+        /// <summary>
+        /// The name of the package to download.
+        /// </summary>
+        public string PackageName { get; set; }
+
+        /// <summary>
+        /// The version of the <see cref="PackageName"/> to download.
+        /// </summary>
+        public string PackageVersion { get; set; }
     }
 }
--- NuGetDownloader/Configuration/DownloaderConfig.cs
+++ NuGetDownloader/Configuration/DownloaderConfig.cs
@@ -5,5 +5,29 @@
     /// </summary>
     public class DownloaderConfig
     {
+        /// <summary>
+        /// The path to the directory in which to download all the packages.
+        /// </summary>
+        public string OutputDirectoryPath { get; set; }
+
+        /// <summary>
+        /// A flag that indicates if to download packages like System of NetStandard etc.
+        /// </summary>
+        public bool DownloadBuiltInLibraries { get; set; }
+
+        /// <summary>
+        /// A list of all the packages to download.
+        /// </summary>
+        public List<DownloadRequest> DownloadRequests { get; set; }
+
+        /// <summary>
+        /// Constructor for initializing new instances of this class.
+        /// </summary>
+        public DownloaderConfig()
+        {
+            OutputDirectoryPath = "./";
+            DownloadBuiltInLibraries = false;
+            DownloadRequests = new List<DownloadRequest>();
+        }
     }
 }
