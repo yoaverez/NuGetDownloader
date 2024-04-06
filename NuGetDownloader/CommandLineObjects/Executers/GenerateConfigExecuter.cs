90b86414d84ccd75868833007120507ed5910933
yoaverez <yoav.erez.01@gmail.com>
2024-04-06 12:31:25 +0300

    Add empty files for the command line executers

---
+++ NuGetDownloader/CommandLineObjects/Executers/GenerateConfigExecuter.cs
@@ -0,0 +1,9 @@
+﻿namespace NuGetDownloader.CommandLineObjects.Executers
+{
+    /// <summary>
+    /// Executer for the "generate-config" command line verb.
+    /// </summary>
+    public static class GenerateConfigExecuter
+    {
+    }
+}
